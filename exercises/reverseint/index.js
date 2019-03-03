caac850561e54f3812a37ba9145c109017514099
Pravin Desai <pravindesai0790@gmail.com>
2019-03-03 21:40:57 +0530

    Reverse Int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,24 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    const reversedNum =  n.toString()
+                          .split('')
+                          .reverse()
+                          .join('');
+                          
+    return parseInt(reversedNum) * Math.sign(n);
+}
+
+//reverseInt(234);
 
 module.exports = reverseInt;
+
+// function reverseInt(n) {
+//     var reverse = parseInt(n.toString().split('').reverse().join(''));
+
+//     if(n < 0)
+//         return reverse * -1;
+
+//     return reverse;
+// }
