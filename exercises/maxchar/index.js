32ec505a1da0887e0d86a9fcf235b44cafa1053a
Pravin Desai <pravindesai0790@gmail.com>
2019-05-10 21:16:13 +0530

    Find max char from string

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,34 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+
+function maxChar(str) {
+    const charMap = {};
+    let max = 0;
+    let maxChar = '';
+
+    for (let char of str) {
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+
+    // console.log(chars);
+    // let arr = Object.keys(chars);
+    // let min = Math.min(...arr);
+    // let max = Math.max(...arr);
+    // console.log(arr);
+    // console.log(`Min value: ${min}, max value: ${max}`);
+
+    for (let key in charMap) {
+        if (charMap[key] > max) {
+            max = charMap[key];
+            maxChar = key;
+        }
+    }
+
+    //console.log('Key: ' + maxChar + ' Value: ' + max);
+    return maxChar;
+}
+
+//console.log(maxChar("abcccccccd"));
 
 module.exports = maxChar;
